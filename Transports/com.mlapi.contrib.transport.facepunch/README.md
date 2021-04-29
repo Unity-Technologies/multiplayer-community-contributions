2788f30ee582475ee33b3264aa278b95de066fa0
BuildTools <mgfloris0106@gmail.com>
2021-04-29 22:16:26 +0200

    Initial commit for Facepunch Transport

---
+++ Transports/com.mlapi.contrib.transport.facepunch/README.md
@@ -0,0 +1,5 @@
+Facepunch Transport for MLAPI
+
+by Nico Thomas, Floris van Onna
+
+credits to Garry Newman (Author of Facepunch.Steamworks)
---
+++ Transports/com.mlapi.contrib.transport.facepunch/Runtime/ReadOnlyAttribute.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace MLAPI.Transports.Facepunch
+{
+    public class ReadOnlyAttribute : PropertyAttribute { }
+
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var previousGUIState = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+            GUI.enabled = previousGUIState;
+        }
+    }
+}
