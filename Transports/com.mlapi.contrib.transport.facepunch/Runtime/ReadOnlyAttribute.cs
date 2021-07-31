af1671410f9cc3f44068cc555a80cad57c81eb58
Luke Stampfli <luke@stampfli.one>
2021-07-31 14:18:38 +0100

    fix: Wrap editor stuff in ifdefs to allow for builds

--- Transports/com.mlapi.contrib.transport.facepunch/Runtime/ReadOnlyAttribute.cs
+++ Transports/com.mlapi.contrib.transport.facepunch/Runtime/ReadOnlyAttribute.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 
 namespace MLAPI.Transports.Facepunch
 {
     public class ReadOnlyAttribute : PropertyAttribute { }
 
+#if UNITY_EDITOR
     [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
     public class ReadOnlyDrawer : PropertyDrawer
     {
@@ -16,4 +19,5 @@ namespace MLAPI.Transports.Facepunch
             GUI.enabled = previousGUIState;
         }
     }
+#endif
 }
