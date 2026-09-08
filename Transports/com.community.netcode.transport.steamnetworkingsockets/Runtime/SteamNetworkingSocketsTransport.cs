0fab638470379ace12b0149dfb41c043d23dbce5
Simon Lemay <simon.lemay@unity3d.com>
2026-09-08 15:15:39 -0400

    Make all transports work with UTP's built-in support

--- Transports/com.community.netcode.transport.steamnetworkingsockets/Runtime/SteamNetworkingSocketsTransport.cs
+++ Transports/com.community.netcode.transport.steamnetworkingsockets/Runtime/SteamNetworkingSocketsTransport.cs
@@ -1,4 +1,4 @@
-﻿#if !DISABLESTEAMWORKS && STEAMWORKSNET && NETCODEGAMEOBJECTS
+﻿#if !DISABLESTEAMWORKS && STEAMWORKSNET
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
